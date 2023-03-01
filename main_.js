624722d13ed35d170d823dea0873ee523f5703de
HanJaekyoung <102638110+HanJaekyoung@users.noreply.github.com>
2023-03-01 11:19:20 +0900

    commit

---
+++ main_.js
@@ -0,0 +1,44 @@
+1.//number, string, boolean, null, undefined
+let num = 3;
+let num2 = num;
+console.log(num);
+console.log(num2);
+
+num2 =4;
+console.log(num);
+console.log(num2)
+console.clear();
+2.//object
+let obj = {
+    name: 'mikoto',
+    age: '16',
+    strength: 'S'
+}
+console.log(obj.strength);
+
+let obj2 = obj;
+console.log(obj.age);
+console.log(obj2.age);
+
+obj.name = 'kurume'
+console.log('-------');
+console.log(obj.name);
+console.log(obj2.name);
+
+let x = 2;
+console.log(x);
+x = 90;
+console.log(x);
+
+const a = 5;
+console.log(a);
+
+
+const obj3 = {
+    name: 'kirito',
+    age: '15'
+};
+console.log(obj3);
+obj3.name = 'Jasen'
+console.log(obj3);
+
